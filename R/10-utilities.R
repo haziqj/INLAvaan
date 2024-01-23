c276a31de81a2d41bd57c4d3ddb910173f0c1e41
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-01-23 18:26:33 +0800

    Seems successful -- but not scaleable!
    In INLA, best not to "marginalise" Sigma

--- R/10-utilities.R
+++ R/10-utilities.R
@@ -2,49 +2,97 @@
 # 2. From PT to Sigma and hence Q
 # 3. From PT to log priors
 
-theta_to_PT <- function() {
-  idx_free <- which(PT$free > 0)
-  PT$est <- PT$ustart
-  PT$est[idx_free] <- theta
-  PT
-}
+# get_p <- function(.PT) {
+#   nrow(PT_to_matrices(.PT = .PT)$Theta)
+# }
+#
+# theta_to_PT <- function(.PT) {
+#
+#   PT <- .PT
+#   idx_free <- which(PT$free > 0)
+# print(theta)
+#   par_vals <- theta
+#   names(par_vals) <- PT$mat[idx_free]
+#   par_vals[names(par_vals) %in% c("theta", "psi")] <- exp(
+#     par_vals[names(par_vals) %in% c("theta", "psi")]
+#   )
+#   PT$est[idx_free] <- par_vals
+#
+#   PT
+# }
+#
+# PT_to_matrices <- function(.PT) {
+#   # Lambda
+#   lambdas <- .PT[grep("lambda", .PT$mat), c("est", "row", "col")]
+#   Lambda <- Matrix::sparseMatrix(i = lambdas$row,
+#                                  j = lambdas$col,
+#                                  x = lambdas$est)
+#
+#   # Theta
+#   thetas <- .PT[grep("theta", .PT$mat), c("est", "row", "col")]
+#   Theta <- Matrix::sparseMatrix(i = thetas$row,
+#                                 j = thetas$col,
+#                                 x = thetas$est)
+#   Thetat <- Matrix::sparseMatrix(j = thetas$row,
+#                                  i = thetas$col,
+#                                  x = thetas$est)
+#   Theta <- (Theta + Thetat) / 2
+#
+#   # Psi
+#   psis <- .PT[grep("psi", .PT$mat), c("est", "row", "col")]
+#   Psi <- Matrix::sparseMatrix(i = psis$row,
+#                               j = psis$col,
+#                               x = psis$est)
+#
+#   list(
+#     Lambda = Lambda,
+#     Theta = Theta,
+#     Psi = Psi
+#   )
+# }
+#
+# # Don't forget to write test functions!
+# parse_dist <- function(x, debug = FALSE) {
+#   if (grepl("normal", x)) {
+#     res <- strsplit(x, ",|\\(|\\)")[[1]]
+#     mean <- as.numeric(res[2])
+#     sd <- sqrt(as.numeric(res[3]))
+#     if (isTRUE(debug)) {
+#       return(list(dist = "normal", mean = mean, sd = sd))
+#     } else {
+#       return(function(theta) dnorm(theta, mean = mean, sd = sd, log = TRUE))
+#     }
+#   }
+#
+#   if (grepl("gamma", x)) {
+#     res <- strsplit(x, ",|\\(|\\)")[[1]]
+#     shape <- as.numeric(res[2])
+#     rate <- as.numeric(res[3])
+#     # FIXME: something to do with [sd] or not?
+#     if (isTRUE(debug)) {
+#       return(list(dist = "gamma", shape = shape, rate = rate))
+#     } else {
+#       return(function(theta) dgamma(theta, shape = shape, rate = rate,
+#                                     log = TRUE))
+#     }
+#
+#     # FIXME: Also, since we parameterise as var = log(theta), need to add Jacobian
+#   }
+#
+#   if (grepl("beta", x)) {
+#     res <- strsplit(x, ",|\\(|\\)")[[1]]
+#     shape1 <- as.numeric(res[2])
+#     shape2 <- as.numeric(res[3])
+#     # FIXME: something to do with [sd] or not?
+#     if (isTRUE(debug)) {
+#       return(list(dist = "beta", shape1 = shape1, shape2 = shape2))
+#     } else {
+#       return(function(theta) dgamma(theta, shape1 = shape1, shape2 = shape2,
+#                                     log = TRUE))
+#     }
+#   }
+# }
 
-PT_to_matrices <- function() {
-  .PT <- theta_to_PT()
-
-  # Lambda
-  lambdas <- .PT[grep("lambda", .PT$mat), c("est", "row", "col")]
-  Lambda <- Matrix::sparseMatrix(i = lambdas$row,
-                                 j = lambdas$col,
-                                 x = lambdas$est)
-
-  # Theta
-  thetas <- .PT[grep("theta", .PT$mat), c("est", "row", "col")]
-  Theta <- Matrix::sparseMatrix(i = thetas$row,
-                                j = thetas$col,
-                                x = thetas$est)
-  Theta <- (Theta + t(Theta)) / 2
-
-  # Psi
-  psis <- .PT[grep("psi", .PT$mat), c("est", "row", "col")]
-  Psi <- Matrix::sparseMatrix(i = psis$row,
-                              j = psis$col,
-                              x = psis$est)
-
-  list(
-    Lambda = Lambda,
-    Theta = Theta,
-    Psi = Psi
-  )
-}
-
-# Don't forget to write test functions!
-parse_dist <- function(x) {
-  if (grepl("normal", x)) {
-    res <- strsplit(x, ",|\\(|\\)")[[1]]
-    mean <- as.numeric(res[2])
-    sd <- sqrt(as.numeric(res[3]))
-    return(function(theta) dnorm(theta, mean = mean, sd = sd, log = TRUE))
-  }
-}
-
+# parse_dist("normal(0,10)", TRUE)
+# parse_dist("gamma(1,.5)[sd]", TRUE)
+# parse_dist("beta(1,1)", TRUE)
--- R/30-inlavaan.R
+++ R/30-inlavaan.R
@@ -1,24 +1,10 @@
-model <- '
-   # latent variable definitions
-     ind60 =~ x1 + x2 + x3
-     dem60 =~ y1 + y2 + y3 + y4
-     dem65 =~ y5 + y6 + y7 + y8
-   # regressions
-     dem60 ~ ind60
-     dem65 ~ ind60 + dem60
-   # residual covariances
-     y1 ~~ y5
-     y2 ~~ y4 + y6
-     y3 ~~ y7
-     y4 ~~ y8
-     y6 ~~ y8
-'
 
 inlavaan <- function(model = NULL,
                      data = NULL) {
 
-  PT <- lavaanify(model, auto = TRUE)
+  PT <- lavaan::lavaanify(model, auto = TRUE)
   PT <- cbind(PT, as.data.frame(lavaan:::lav_lisrel(PT)))
+  PT$est <- PT$ustart
 
   # Priors (default?)
   PT <- PT |>
@@ -34,19 +20,186 @@ inlavaan <- function(model = NULL,
       mat == "tau" ~ "normal(0,1.5)"
     ))
 
+  print(PT)
 
-  # dat_inla <-
-  #   dat |>
-  #   mutate(id = row_number()) |>
-  #   pivot_longer(-id, names_to = "item", values_to = "y") |>
-  #   mutate(k = row_number())
+  n <- nrow(data)
+  # Stretch the data
+  dat_inla <-
+    data |>
+    mutate(iii = row_number()) |>  # FIXME: making sure there isn't a column named iii!
+    pivot_longer(-iii, names_to = "item", values_to = "y") |>
+    mutate(kkk = row_number())  # FIXME: Same thing here with kkk.
 
-  # Formula for INLA
-  # form = y ~ -1 + f(k, model = model)
 
-  # inla(form, data = dat_inla,
-  #      control.family = list(hyper = list(prec = list(initial = 15,
-  #                                                     fixed = TRUE))),
-  #      verbose = FALSE)
+  # Create the INLA model
+  inla.rgeneric.sem.model <- function(
+    cmd = c("graph", "Q", "mu", "initial", "log.norm.const", "log.prior",
+            "quit"),
+    theta = NULL
+  ) {
 
-}; inlavaan(model)
+    get_p <- function() {
+      nrow(PT_to_matrices(PT)$Theta)
+    }
+
+    theta_to_PT <- function() {
+
+      idx_free <- which(PT$free > 0)
+
+      par_vals <- theta
+      names(par_vals) <- PT$mat[idx_free]
+      par_vals[names(par_vals) %in% c("theta", "psi")] <- exp(
+        par_vals[names(par_vals) %in% c("theta", "psi")]
+      )
+      PT$est[idx_free] <- par_vals
+
+      PT
+    }
+
+    PT_to_matrices <- function(.PT) {
+      # Lambda
+      lambdas <- .PT[grep("lambda", .PT$mat), c("est", "row", "col")]
+      Lambda <- Matrix::sparseMatrix(i = lambdas$row,
+                                     j = lambdas$col,
+                                     x = lambdas$est)
+
+      # Theta
+      thetas <- .PT[grep("theta", .PT$mat), c("est", "row", "col")]
+      Theta <- Matrix::sparseMatrix(i = thetas$row,
+                                    j = thetas$col,
+                                    x = thetas$est)
+      Theta <- (Theta + Matrix::t(Theta)) / 2
+
+      # Psi
+      psis <- .PT[grep("psi", .PT$mat), c("est", "row", "col")]
+      Psi <- Matrix::sparseMatrix(i = psis$row,
+                                  j = psis$col,
+                                  x = psis$est)
+
+      list(
+        Lambda = Lambda,
+        Theta = Theta,
+        Psi = Psi
+      )
+    }
+
+    # Don't forget to write test functions!
+    parse_dist <- function(x, debug = FALSE) {
+      if (grepl("normal", x)) {
+        res <- strsplit(x, ",|\\(|\\)")[[1]]
+        mean <- as.numeric(res[2])
+        sd <- sqrt(as.numeric(res[3]))
+        if (isTRUE(debug)) {
+          return(list(dist = "normal", mean = mean, sd = sd))
+        } else {
+          return(function(theta) dnorm(theta, mean = mean, sd = sd, log = TRUE))
+        }
+      }
+
+      if (grepl("gamma", x)) {
+        res <- strsplit(x, ",|\\(|\\)")[[1]]
+        shape <- as.numeric(res[2])
+        rate <- as.numeric(res[3])
+        # FIXME: something to do with [sd] or not?
+        if (isTRUE(debug)) {
+          return(list(dist = "gamma", shape = shape, rate = rate))
+        } else {
+          return(function(theta) dgamma(theta, shape = shape, rate = rate,
+                                        log = TRUE))
+        }
+
+        # FIXME: Also, since we parameterise as var = log(theta), need to add Jacobian
+      }
+
+      if (grepl("beta", x)) {
+        res <- strsplit(x, ",|\\(|\\)")[[1]]
+        shape1 <- as.numeric(res[2])
+        shape2 <- as.numeric(res[3])
+        # FIXME: something to do with [sd] or not?
+        if (isTRUE(debug)) {
+          return(list(dist = "beta", shape1 = shape1, shape2 = shape2))
+        } else {
+          return(function(theta) dgamma(theta, shape1 = shape1, shape2 = shape2,
+                                        log = TRUE))
+        }
+      }
+    }
+
+    Q <- function() {
+      new_PT <- theta_to_PT()
+      mat <- PT_to_matrices(new_PT)
+      Lambda <- mat$Lambda
+      Theta <- mat$Theta
+      Psi <- mat$Psi
+      Sigma <- Lambda %*% Psi %*% Matrix::t(Lambda) + Theta
+      Q <- solve(Sigma)
+      p <- get_p()
+
+      Qlist <- replicate(n, Q, simplify = FALSE)
+      Q <- bdiag(Qlist)
+      inla.as.sparse(Q)
+      # rows <- rep(which(Q != 0, arr.ind = TRUE)[,1], n)
+      # cols <- rep(which(Q != 0, arr.ind = TRUE)[,2], n)
+      # vals <- rep(Q[Q != 0], n)
+      #
+      # print(Sigma)
+      #
+      #
+      # # Adjust indices for block diagonal structure
+      # for(i in 1:(n-1)) {
+      #   rows[((i*p*p)+1):((i+1)*p*p)] <- rows[((i*p*p)+1):((i+1)*p*p)] + i*p
+      #   cols[((i*p*p)+1):((i+1)*p*p)] <- cols[((i*p*p)+1):((i+1)*p*p)] + i*p
+      # }
+      #
+      # # Create the block diagonal sparse matrix
+      # sparseMatrix(i = rows, j = cols, x = vals, dims = c(n*p, n*p))
+    }
+
+    mu <- function() { numeric(0) }  # FIXME: When meanstructure = TRUE then this should be a vector of length p
+
+    log.norm.const <- function() { numeric(0) }
+
+    log.prior <- function() {
+      require("tidyverse")
+      theta_to_PT() |>
+        as_tibble() |>
+        select(est, prior) |>
+        mutate(prior = map(prior, parse_dist)) |>
+        mutate(vals = purrr::map2_dbl(prior, est, ~ .x(.y))) |>
+        pull(est) |>
+        sum()
+    }
+
+    initial <- function() {
+      nparam <- sum(PT$free > 0)
+      rnorm(nparam)
+    }
+
+    graph <- function() {
+      p <- get_p()
+      Qlist <- replicate(n, matrix(1, nrow = p, ncol = p), simplify = FALSE)
+      Q <- bdiag(Qlist)
+      inla.as.sparse(Q)
+    }
+
+    quit <- function() { return(invisible()) }
+
+    if (!length(theta)) theta = initial()
+    val = do.call(match.arg(cmd), args = list())
+    return (val)
+  }
+
+  inlavaan_model <- inla.rgeneric.define(inla.rgeneric.sem.model,
+                                         PT = PT,
+                                         n = nrow(data))
+
+
+
+
+  inla(y ~ -1 + f(kkk, model = inlavaan_model),
+       data = dat_inla,
+       control.family = list(hyper = list(prec = list(initial = 15,
+                                                      fixed = TRUE))),
+       verbose = FALSE)
+
+}
