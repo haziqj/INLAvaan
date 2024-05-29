f4b225a2285ee43f84282a942e719ff8fdeea7c6
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-05-29 11:18:46 +0300

    Test rgeneric model

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -1,20 +1,53 @@
 test_that("Fake test", {
   require(INLA)
 
+  rgeneric.test = function(
+    cmd = c("graph", "Q", "mu", "initial", "log.norm.const", "log.prior", "quit"),
+    theta = NULL)
+  {
+    envir = parent.env(environment())
 
-  n = 100; a = 1; b = 1; tau = 100
-  z = rnorm(n)
-  eta = a + b*z
+    graph = function() {
+      return(matrix(1, n, n))
+    }
 
-  scale = exp(rnorm(n))
-  prec = scale*tau
-  y = rnorm(n, mean = eta, sd = 1/sqrt(prec))
+    Q = function() {
+      R <- matrix(sin(1:n^2), n, n)
+      R <- R %*% t(R)
+      diag(R) <- diag(R)+1
+      Q <- exp(theta[1]) * R
+      return(Q)
+    }
 
-  data = list(y=y, z=z)
-  formula = y ~ 1+z
+    mu = function() { return(numeric(0)) }
+
+    log.norm.const = function() { return(numeric(0)) }
+
+    log.prior = function() {
+      return (dgamma(exp(theta[1]), shape = 1, rate = 1, log=TRUE) + theta[1])
+    }
+
+    initial = function() { return(4) }
+
+    if (!length(theta)) theta = initial()
+    val = do.call(match.arg(cmd), args = list())
+    return (val)
+  }
+
+  n = 100
+  s = .1
+  Q <- rgeneric.test("Q", theta = 0)
+  library(mvtnorm)
+  S <- solve(as.matrix(Q))
+  S <- (S + t(S))/2
+  x <- drop(rmvnorm(1, sigma = S))
+  y <- x + rnorm(n, sd = s)
+  cont.family = list(hyper = list(prec = list(initial=log(1/s^2), fixed=TRUE)))
+  model2 = inla.rgeneric.define(rgeneric.test, n=n, optimize = FALSE)
 
   expect_no_error({
-    result = inla(formula, family = "gaussian", data = data)
+    r2 = inla(y ~ -1 + f(idx, model=model2),
+              data = data.frame(y = y, idx = 1:n), control.family = cont.family)
   })
 
 
