d9a70f31d81d0288889d95c1ef7e728b4f7f026e
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2024-05-29 10:47:14 +0300

    Try a simpler test example

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -1,42 +1,64 @@
-test_that("CFA", {
-  INLA::inla.setOption(num.threads = "1:1")
+test_that("Fake test", {
+  require(INLA)
 
-  mod <- "
-    eta1 =~ y1 + y2 + y3
-    eta2 =~ y4 + y5 + y6
-  "
-  dat <- lavaan::simulateData(mod, sample.nobs = 100)
+
+  n = 100; a = 1; b = 1; tau = 100
+  z = rnorm(n)
+  eta = a + b*z
+
+  scale = exp(rnorm(n))
+  prec = scale*tau
+  y = rnorm(n, mean = eta, sd = 1/sqrt(prec))
+
+  data = list(y=y, z=z)
+  formula = y ~ 1+z
+
   expect_no_error({
-    fit <- icfa(
-      model = mod,
-      data = dat,
-      meanstructure = FALSE,
-      verbose = FALSE,
-      npost = 10
-    )
+    result = inla(formula, family = "gaussian", data = data)
   })
+
+
 })
 
-test_that("SEM", {
-  INLA::inla.setOption(num.threads = "1:1")
-
-  mod <- "
-    eta1 =~ y1 + y2 + y3
-    eta2 =~ y4 + y5 + y6
-    eta2 ~ 0.3*eta1
-    y1 ~~ 0.5*y4
-  "
-  dat <- lavaan::simulateData(mod, sample.nobs = 100)
-  expect_no_error({
-    fit <- isem(
-      model = mod,
-      data = dat,
-      meanstructure = FALSE,
-      verbose = FALSE,
-      npost = 10
-    )
-  })
-})
+# test_that("CFA", {
+#   INLA::inla.setOption(num.threads = "1:1")
+#
+#   mod <- "
+#     eta1 =~ y1 + y2 + y3
+#     eta2 =~ y4 + y5 + y6
+#   "
+#   dat <- lavaan::simulateData(mod, sample.nobs = 100)
+#   expect_no_error({
+#     fit <- icfa(
+#       model = mod,
+#       data = dat,
+#       meanstructure = FALSE,
+#       verbose = FALSE,
+#       npost = 10
+#     )
+#   })
+# })
+#
+# test_that("SEM", {
+#   INLA::inla.setOption(num.threads = "1:1")
+#
+#   mod <- "
+#     eta1 =~ y1 + y2 + y3
+#     eta2 =~ y4 + y5 + y6
+#     eta2 ~ 0.3*eta1
+#     y1 ~~ 0.5*y4
+#   "
+#   dat <- lavaan::simulateData(mod, sample.nobs = 100)
+#   expect_no_error({
+#     fit <- isem(
+#       model = mod,
+#       data = dat,
+#       meanstructure = FALSE,
+#       verbose = FALSE,
+#       npost = 10
+#     )
+#   })
+# })
 
 
 
