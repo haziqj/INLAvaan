4470266ff3991563681a5a9aacc9c5810e848bac
Haziq Jamil <haziq.jamil@kaust.edu.sa>
2025-09-10 17:22:00 +0300

    Update hexSticker.R

--- inst/hexSticker.R
+++ inst/hexSticker.R
@@ -5,12 +5,12 @@ sticker(
   p_size = 0,                         # also ensure package text is invisible
   s_x = 1, s_y = 1.3,                 # subplot centre; tweak these
   s_width = 0.8, s_height = 0.8,      # size of the subplot inside hex
-  h_fill = scales::alpha("#EDDAAB", 0.15),                 # hex background colour
+  h_fill = scales::alpha("#fcf9f2", 1),                 # hex background colour
   h_color = "#2b2b2b",                # hex border colour (set to NA to hide)
   h_size = 0.5,
   filename = "inst/hex_inlavaan.png",
   dpi = 900,
-  white_around_sticker = FALSE
+  white_around_sticker = TRUE
 )
 
 # Needs manual edit to remove the excess outside the hex
