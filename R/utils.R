18a030183c7d16494e470586d996ee739129103d
Haziq Jamil <haziq.jamil@kaust.edu.sa>
2025-12-02 10:28:39 +0300

    Move cli_messages to utils.R

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+cli_messages <- c(
+  "Laplace-ing through p dimensions",
+  "Summoning Bayesian spirits",
+  "Casting statistical spells",
+  "Conjuring INLA magic",
+  "Channeling Laplace's wizardry",
+  "Harnessing the power of priors",
+  "Diving into the probability pool",
+  "Navigating the seas of stochasticity"
+)
+
