53297d888c7e28ae11a12e6967c85d5b9c803735
Haziq Jamil <haziq.jamil@kaust.edu.sa>
2026-01-14 15:04:25 +0300

    Update docs

--- R/INLAvaan-package.R
+++ R/INLAvaan-package.R
@@ -1,4 +1,24 @@
 #' @keywords internal
+#' @section Main features:
+#' * [acfa()]: Approximate Confirmatory Factor Analysis.
+#' * [asem()]: Approximate Structural Equation Modelling.
+#' * [agrowth()]: Approximate Latent Growth Curve models.
+#'
+#' @section Model specifications:
+#' Supports advanced 'lavaan' syntax features, including:
+#' * Equality constraints
+#' * Defined parameters (e.g., `:=` operator for indirect effects)
+#' * Flexible prior specifications
+#'
+#' @section Online vignettes:
+#' The [package website](https://inlavaan.haziqj.ml/) contains comprehensive examples covering:
+#' * Confirmatory Factor Analysis (CFA)
+#' * Structural Equation Models (SEM)
+#' * Latent Growth Curve Models
+#' * Multigroup and Invariance Testing
+#' * Mediation Analysis
+#'
+#' @aliases INLAvaan
 "_PACKAGE"
 
 ## usethis namespace: start
