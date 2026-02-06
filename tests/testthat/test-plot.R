416d800a4ee248f9da616283e458e34e887b9c72
Haziq Jamil <haziq.jamil@kaust.edu.sa>
2026-02-06 17:20:25 +0100

    Not so easy to remove ggplot2 yet

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -5,7 +5,9 @@ test_that("Plot method works", {
     textual =~ x4 + x5 + x6
   "
   fit <- acfa(mod, dat, verbose = FALSE, nsamp = 3, test = "none")
-  expect_no_error(plt <- plot(fit))
-  expect_no_error(plt <- plot(fit, truth = coef(fit)))
-  expect_equal(length(plt@layers), length(coef(fit)))
+  pdf(file = tempfile())
+  expect_silent(plt <- plot(fit))
+  expect_silent(plt <- plot(fit, truth = coef(fit)))
+  dev.off()
+  # expect_equal(length(plt@layers), length(coef(fit)))
 })
